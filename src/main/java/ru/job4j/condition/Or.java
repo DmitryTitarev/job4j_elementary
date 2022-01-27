31308e552027d1c31c7053b0ae4f0baca985f974
Dmitry <d.titarev1@gmail.com>
2022-01-27 19:27:53 +0100

    4.3.2. Оператор || [#333784]

---
+++ src/main/java/ru/job4j/condition/Or.java
@@ -0,0 +1,11 @@
+package ru.job4j.condition;
+
+public class Or {
+
+    public static void main(String[] args) {
+        boolean first = false;
+        boolean second = false;
+        boolean join = first || second;
+        System.out.println(join);
+    }
+}
